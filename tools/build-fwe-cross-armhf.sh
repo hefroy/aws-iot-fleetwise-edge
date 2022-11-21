444e85141cc28db961386d32457eaae2ce0d6592
Geoff Phillips <phigeoff@amazon.de>
2022-11-21 15:39:34 +0100

    test

--- tools/build-fwe-cross-armhf.sh
+++ tools/build-fwe-cross-armhf.sh
@@ -7,7 +7,6 @@ set -eo pipefail
 mkdir -p build && cd build
 cmake \
   -DFWE_STATIC_LINK=On \
-  -DFWE_STRIP_SYMBOLS=On \
   -DFWE_SECURITY_COMPILE_FLAGS=On \
   -DCMAKE_TOOLCHAIN_FILE=/usr/local/arm-linux-gnueabihf/lib/cmake/armhf-toolchain.cmake \
   -DBUILD_TESTING=Off \
